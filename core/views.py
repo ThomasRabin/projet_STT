8a81698c86dee3cfd488777307965bb24d068117
Thomas Rabin <thomas.rabin@cicor.com>
2026-05-19 10:03:03 +0200

    Ajout de filtre sur page logs + modif view

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.utils import timezone
 
 from .models import OF, LogTest, Produit, ReferenceProduit, PassageTest
+from django.utils.html import escape
 
 def index(request):
     return HttpResponse("Hello, world. You're at the core index.")
@@ -23,23 +24,64 @@ def produit_list(request):
 
 def logs_list(request):
     """
-    Affiche tous les logs présents en base.
+    Affiche les logs présents en base.
+    Filtre optionnel par numéro de série produit via ?sn=...
+    Exemple :
+        /core/logs/?sn=1234567000005
     """
 
-    logs = LogTest.objects.all().order_by("-dateEtape")
+    sn = request.GET.get("sn", "").strip()
+
+    logs = (
+        LogTest.objects
+        .select_related("idTest", "idTest__idProduit")
+        .all()
+        .order_by("-dateEtape")
+    )
+
+    if sn:
+        logs = logs.filter(idTest__idProduit__SN=sn)
 
     lignes = []
 
+    lignes.append(
+        f"""
+        <form method="get" style="margin-bottom:20px;">
+            <label for="sn"><b>Filtrer par SN :</b></label>
+            <input type="text" id="sn" name="sn" value="{escape(sn)}">
+            <button type="submit">Filtrer</button>
+            <a href="{request.path}">Réinitialiser</a>
+        </form>
+        <hr>
+        """
+    )
+
+    if not logs.exists():
+        lignes.append(
+            f"""
+            <p>Aucun log trouvé{f" pour le SN {escape(sn)}" if sn else ""}.</p>
+            """
+        )
+
     for log in logs:
+        if log.dateEtape:
+            date_locale = timezone.localtime(log.dateEtape)
+            date_formattee = date_locale.strftime("%d/%m/%Y %H:%M:%S")
+        else:
+            date_formattee = "N/A"
+
         lignes.append(
             f"""
             <div style="margin-bottom:15px;">
-                <b>Test ID :</b> {log.idTest.idRapport}<br>
-                <b>Étape :</b> {log.numeroEtape} - {log.nomEtape}<br>
+                <b>Rapport :</b> {escape(log.idTest.idRapport or "")}<br>
+                <b>SN :</b> {escape(log.idTest.idProduit.SN or "")}<br>
+                <b>Étape :</b> {escape(str(log.numeroEtape))} - {escape(log.nomEtape or "")}<br>
                 <b>Résultat :</b> {log.resultatEtape}<br>
-                <b>Composant :</b> {log.composant}<br>
-                <b>Face :</b> {log.face}<br>
-                <b>Date :</b> {log.dateEtape}
+                <b>Composant :</b> {escape(log.composant or "")}<br>
+                <b>Référence composant :</b> {escape(log.refComposant or "")}<br>
+                <b>Face :</b> {escape(log.face or "")}<br>
+                <b>Position :</b> {log.position}<br>
+                <b>Date :</b> {date_formattee}
             </div>
             <hr>
             """
@@ -69,6 +111,8 @@ def tests(request):
                 <b>Flag FPY :</b> {test.FPY_flag}<br>
                 <b>Machine :</b> {test.idOperation.idMachine}<br>
                 <b>Interface :</b> {test.idInterfaceUtilisee}<br>
+                <b>Version logiciel :</b> {test.versionLogiciel}<br>
+                <b>Opérateur :</b> {test.codeOperateur}<br>
             </div>
             <hr>
             """
